c26fb8790c6a5565120f4f6bd32a06b611ff13b6
angelinabriciene <aburbaite865@gmail.com>
2024-06-27 10:37:33 +0300

    + skelbimo įkelimas

--- src/test/java/ElentaTests.java
+++ src/test/java/ElentaTests.java
@@ -8,6 +8,9 @@ import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.awt.*;
+import java.awt.datatransfer.StringSelection;
+import java.awt.event.KeyEvent;
 import java.security.SecureRandom;
 import java.time.Duration;
 import java.util.Random;
@@ -31,6 +34,7 @@ public class ElentaTests {
         driver.get("https://elenta.lt");
         driver.findElement(By.className("fc-cta-consent")).click();
     }
+
     private String getRandomLetters(int length) {
         Random random = new Random();
         StringBuilder sb = new StringBuilder();
@@ -40,6 +44,7 @@ public class ElentaTests {
         }
         return sb.toString();
     }
+
     public String getRandomSymbols(int length) {
         String symbols = "!@#$%^&*()_+-={}:<>?[]|";
         StringBuilder randomSymbols = new StringBuilder();
@@ -52,6 +57,7 @@ public class ElentaTests {
 
         return randomSymbols.toString();
     }
+
     public String getRandomSymbolsLT(int length) {
         String symbols = "ĄČĘĖĮŠŲŪ";
         StringBuilder randomSymbolsLT = new StringBuilder();
@@ -67,18 +73,19 @@ public class ElentaTests {
 
     @BeforeMethod
     public void navigateToRegistrationPage() {
-        driver.get("https://elenta.lt/registracija");
+//        driver.get("https://elenta.lt/registracija");
+        driver.get("https://elenta.lt");
     }
 
     @Test
     public void normalRegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -94,6 +101,7 @@ public class ElentaTests {
         WebElement registrationComfirmed = driver.findElement(By.className("info"));
         Assert.assertTrue(registrationComfirmed.isDisplayed());
     }
+
     @Test
     public void emptyUsernameRegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
@@ -102,7 +110,7 @@ public class ElentaTests {
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -118,6 +126,7 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void twoLettersUsernameRegistrationTest() {
         String randomLetters = getRandomLetters(2);
@@ -127,7 +136,7 @@ public class ElentaTests {
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -143,6 +152,7 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void thirtyLettersUsernameRegistrationTest() {
         String randomLetters = getRandomLetters(30);
@@ -152,7 +162,7 @@ public class ElentaTests {
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -168,6 +178,7 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void specialSymbolsUsernameRegistrationTest() {
         String randomSymbols = getRandomSymbols(8);
@@ -177,7 +188,7 @@ public class ElentaTests {
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -193,6 +204,7 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void specialLTSymbolsUsernameRegistrationTest() {
         String randomSymbolsLT = getRandomSymbolsLT(8);
@@ -202,7 +214,7 @@ public class ElentaTests {
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -218,15 +230,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[1]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void emptyPassword1RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -242,15 +255,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void sixSymbolsPassword1RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -266,15 +280,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void allLowerCasePassword1RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -290,15 +305,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void allUpperCasePassword1RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -314,15 +330,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void noNumberPassword1RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -338,15 +355,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void noLetterPassword1RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -362,15 +380,16 @@ public class ElentaTests {
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[7]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
     }
+
     @Test
     public void notMatchingPassword1ToPassword2RegistrationTest() {
         WebElement username = driver.findElement(By.id("UserName"));
         username.click();
-        username.sendKeys("Testas" + Math.round(Math.random()*300) + "as" + Math.round(Math.random()*300) + Math.round(Math.random()*300));
+        username.sendKeys("Testas" + Math.round(Math.random() * 300) + "as" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300));
 
         WebElement email = driver.findElement(By.id("Email"));
         email.click();
-        email.sendKeys("teastas" + Math.round(Math.random()*300) + Math.round(Math.random()*300) + "@testas" + Math.round(Math.random()*300) + ".lt");
+        email.sendKeys("teastas" + Math.round(Math.random() * 300) + Math.round(Math.random() * 300) + "@testas" + Math.round(Math.random() * 300) + ".lt");
 
         WebElement password = driver.findElement(By.id("Password"));
         password.click();
@@ -385,6 +404,63 @@ public class ElentaTests {
 
         WebElement noUserName = driver.findElement(By.xpath("//*[@id=\"main-container\"]/form/fieldset/table/tbody/tr[8]/td[2]/span"));
         Assert.assertTrue(noUserName.isDisplayed());
+    }
+
+    @Test
+    public void addNewAutoAndMotoSellAdvertNormalTest() throws AWTException, InterruptedException {
+        driver.findElement(By.id("submit-new-ad-nav-button")).click();
+        driver.findElement(By.xpath("//*[@id=\"select-top-category-list\"]/li[1]/a")).click();
+        driver.findElement(By.xpath("//*[@id=\"select-sub-category-list\"]/li[1]/a")).click();
+
+        WebElement advertName = driver.findElement(By.id("title"));
+        advertName.click();
+        advertName.sendKeys(getRandomLetters(10));
+
+        WebElement advertDescription = driver.findElement(By.id("text"));
+        advertDescription.click();
+        advertDescription.sendKeys(getRandomLetters(10), getRandomSymbols(2), getRandomSymbolsLT(10));
+
+        WebElement advertPrice = driver.findElement(By.id("price"));
+        advertPrice.click();
+        advertPrice.sendKeys("100");
+
+        WebElement advertCity = driver.findElement(By.id("location-search-box"));
+        advertCity.click();
+        advertCity.sendKeys("Kaunas");
+
+        WebElement advertPhoNoo = driver.findElement(By.id("phone"));
+        advertPhoNoo.click();
+        advertPhoNoo.sendKeys("+37060011223");
+
+        WebElement advertEmail = driver.findElement(By.id("email"));
+        advertEmail.click();
+        advertEmail.sendKeys("testas@gmail.com");
+
+        WebElement advertSecndLevel = driver.findElement(By.id("submit-button"));
+        advertSecndLevel.click();
+
+        WebElement uploadPhoto = driver.findElement(By.id("fileinput-label"));
+        uploadPhoto.click();
+        String photoPath = "C:\\Users\\Angelina7\\Downloads\\New folder\\vln2.jpg";
+        Robot robot = new Robot();
+        robot.delay(1000);
+        StringSelection selection = new StringSelection(photoPath);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(selection, selection);
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+        Thread.sleep(10000);
+        WebElement advertFinalLevel = driver.findElement(By.id("forward-button"));
+        advertFinalLevel.click();
+
+//        WebElement advertThirdLevel = driver.findElement(By.id("forward-button"));
+//        advertThirdLevel.click();
+
+        WebElement advertLink = driver.findElement(By.xpath("//*[@id=\"main-container\"]/h1"));
+        Assert.assertTrue(advertLink.isDisplayed());
     }
 
     @AfterClass
